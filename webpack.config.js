858d0353e508ea5660b3be6b66f8b382c3ca33d8
ShawnRib <85851503+ShawnRib@users.noreply.github.com>
2021-06-13 18:10:44 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const IS_DEVELOPMENT = process.env.NODE_ENV == 'dev'
+
+const dirApp = path.join(__dirname, 'app')
+const dirShared = path.join(__dirname, 'shared')
+const dirStyles = path.join(__dirname, 'styles')
+const dirNode = 'node_modules'
+
+console.log(dirApp, dirShared, dirStyles)
+
+module.exports = {
+    entry: [
+        path.join(dirApp, 'index.js'),
+        path.join(dirStyles, 'index.scss')
+    ],
+
+    resolve: {
+        module: [
+            dirApp,
+            dirShared,
+            dirStyles,
+            dirNode
+        ]
+    },
+
+    plugins: [
+        new webpack.DefinePlugin({
+            IS_DEVELOPMENT
+        }),
+
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: './shared',
+                    to: ''
+                }
+            ]
+        })
+    ]
+}
